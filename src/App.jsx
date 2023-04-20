e12e35736b789b1c780896ecad1c6deb13209605
Anderson <andersonsilvapp@gmail.com>
2023-04-20 18:28:33 -0300

    feat: add sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import { Header } from './components/Header'
+
+import styles from './App.module.css'
+
 import './global.css'
+import { Sidebar } from './components/Sidebar'
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          posts
+        </main>
+      </div>
+    </>
   )
 }
 
